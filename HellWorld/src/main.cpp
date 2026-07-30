8d6ebe172cf75e505bbd6a9dff0bd6e96a09ab08
skell80r <john.cruickshank@gmail.com>
2026-07-29 21:33:43 -0400

    more lights!

--- HellWorld/src/main.cpp
+++ HellWorld/src/main.cpp
@@ -22,11 +22,11 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
 
   // wait for a second
-  delay(1000);
+  delay(420);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
 
    // wait for a second
-  delay(1000);
+  delay(269);
 }
