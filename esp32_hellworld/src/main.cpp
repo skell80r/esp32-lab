d61e85271b495a3b8dc03423c6c594f57b57c445
skell80r <john.cruickshank@gmail.com>
2026-07-29 22:10:46 -0400

    Working! now with more numbers!

--- esp32_hellworld/src/main.cpp
+++ esp32_hellworld/src/main.cpp
@@ -1,32 +1,15 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-#include "Arduino.h"
+#include <Arduino.h>
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+int x = 0;
 
-void setup()
+void setup() 
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop()
+void loop() 
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+  Serial.println(x);
+  delay(2000);
+  x = x + 1;
 }
