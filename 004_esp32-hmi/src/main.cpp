2867803cec27977e49a8274ab555a9420a517369
skell80r <john.cruickshank@gmail.com>
2026-08-15 21:53:23 -0400

    wip: HMI

--- 004_esp32-hmi/src/main.cpp
+++ 004_esp32-hmi/src/main.cpp
@@ -1,16 +1,66 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_BME680.h>
+#include <LovyanGFX.hpp>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME680 bme;
 
+class LGFX : public lgfx::LGFX_Device
+{
+  lgfx::Panel_ST7796 _panel_instance;
+  lgfx::Bus_SPI _bus_instance;
+  lgfx::Light_PWM _light_instance;
+
+public:
+  LGFX(void)
+  {
+    auto cfg = _bus_instance.config();
+    cfg.spi_host = VSPI_HOST;
+    cfg.spi_mode = 0;
+    cfg.freq_write = 40000000;
+    cfg.freq_read = 16000000;
+    cfg.spi_3wire = true;
+    cfg.dma_channel = SPI_DMA_CH_AUTO;
+    cfg.pin_sclk = 18; // denky32 default VSPI SCK
+    cfg.pin_mosi = 23; // denky32 default VSPI MOSI (SDI)
+    cfg.pin_miso = 19; // denky32 default VSPI MISO (SDO)
+    cfg.pin_dc = -1;   // TODO: set to LCD_RS wiring
+    _bus_instance.config(cfg);
+    _panel_instance.setBus(&_bus_instance);
+
+    auto pcfg = _panel_instance.config();
+    pcfg.pin_cs = -1;  // TODO: set to LCD_CS wiring
+    pcfg.pin_rst = -1; // TODO: set to LCD_RST wiring
+    pcfg.pin_busy = -1;
+    pcfg.panel_width = 320;
+    pcfg.panel_height = 480;
+    _panel_instance.config(pcfg);
+
+    auto bl_cfg = _light_instance.config();
+    bl_cfg.pin_bl = -1; // TODO: set to LED backlight wiring
+    bl_cfg.freq = 44100;
+    bl_cfg.pwm_channel = 7;
+    _light_instance.config(bl_cfg);
+    _panel_instance.setLight(&_light_instance);
+
+    setPanel(&_panel_instance);
+  }
+};
+
+LGFX tft;
 
 /*
 BME680:
   21 -> SDA
   22 -> SCL
+
+LCD (ST7796U, SPI):
+  18 -> SCK
+  23 -> SDI (MOSI)
+  19 -> SDO (MISO)
+  LCD_CS / LCD_RS / LCD_RST / LED -> TODO, fill in tft config above once wired
 
 pio run -t upload and pio device monitor
 */
@@ -35,6 +85,11 @@ void setup()
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
+
+  tft.init();
+  tft.setRotation(1);
+  tft.setBrightness(255);
+  tft.fillScreen(TFT_BLACK);
 }
 
 void loop()
@@ -67,5 +122,14 @@ void loop()
   Serial.println(" m");
 
   Serial.println();
+
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(4);
+  tft.setCursor(20, 20);
+  tft.print("Temp: ");
+  tft.print(bme.temperature, 1);
+  tft.println(" C");
+
   delay(2000);
 }
